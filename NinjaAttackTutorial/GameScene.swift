fd47e6309ff861b7d9f8d025e9d8c0c1a69f94c3
Lucas Dahl <LucasDahl425@yahoo.com>
2019-02-12 12:04:33 -0800

    Setup the player node, made the app iPhone only, along with making it landscape left and right only.

--- NinjaAttackTutorial/GameScene.swift
+++ NinjaAttackTutorial/GameScene.swift
@@ -10,5 +10,24 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    //===================
+    // MARK: - Properties
+    //===================
+    
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    override func didMove(to view: SKView) {
+        
+        // Set the background color of the scene
+        backgroundColor = SKColor.white
+        
+        // Set the player position
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        
+        // Add the player to the scene
+        addChild(player)
+        
+    }
   
 }
