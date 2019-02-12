7289ce6e05b58dd54596b7edb888bd570e3cb95f
Lucas Dahl <LucasDahl425@yahoo.com>
2019-02-12 11:58:43 -0800

    Setup the scene and the SKView.

--- NinjaAttackTutorial/GameViewController.swift
+++ NinjaAttackTutorial/GameViewController.swift
@@ -15,6 +15,26 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup the scene
+        let scene = GameScene(size: view.bounds.size)
+        
+        // Setup the skview
+        let skView = view as! SKView
+        
+        // Shpws the FPS and node count
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        // Ignore other siblings
+        skView.ignoresSiblingOrder = true
+        
+        // Set the scene scale mode
+        scene.scaleMode = .resizeFill
+        
+        // Present the scene
+        skView.presentScene(scene)
+        
+        
     }
 
     override var prefersStatusBarHidden: Bool {
